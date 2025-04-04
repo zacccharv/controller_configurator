f8452428105deed2e06fb3493635a57ceffd120d
zacccharv <zacchien1@gmail.com>
2025-04-03 23:35:06 -0400

    page picker button generate-grid

--- _scripts/controller_data_page/buttons/exit_button.gd
+++ _scripts/controller_data_page/buttons/exit_button.gd
@@ -5,4 +5,4 @@ func _ready():
 
 func _on_pressed():
 	# Swap the page from the controller picker to the controller data page
-	get_node("/root/Main/page_picker").pick_page("controller_picker_page")
+	get_node("/root/Main/page_picker").pick_page("controller_picker_page", " ")
--- _scripts/page_picker.gd
+++ _scripts/page_picker.gd
@@ -1,16 +1,16 @@
-extends Node
+class_name page_picker extends Node
+
+signal page_picked(data: Variant)
 
 @export var pages: Dictionary[String, PackedScene]
 @export var default_page: String
 @onready var page_root = get_node("./page_root")
 
 func _ready():
-	# for page in pages:
-	# 	(pages[page] as Resource).set_path_cache(page)
 	if pages.has(default_page):
-		pick_page(default_page)
+		pick_page(default_page, "")
 
-func pick_page(page_name: String):
+func pick_page(page_name: String, data: Variant):
 	if get_child_count() > 0:
 		var children = get_children()
 		for child in children:
@@ -22,3 +22,4 @@ func pick_page(page_name: String):
 	page = page_res.instantiate()
 
 	add_child(page)
+	page_picked.emit(data)
